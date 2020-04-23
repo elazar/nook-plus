b61d4313baf8eca0367460cd87eb73d97f3b805c
elazar <tobias382@gmail.com>
2020-04-23 16:32:48 -0500

    Add icons for iOS

--- scripts/icons.sh
+++ scripts/icons.sh
@@ -5,3 +5,9 @@ for d in 48 72 96 144 168 192; do
 done
 
 docker-compose run imagemagick convert homescreen.png -background none -resize 128x128 -density 128x128 build/favicon.ico
+
+# https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html
+# https://developer.apple.com/design/human-interface-guidelines/ios/icons-and-images/app-icon/#app-icon-sizes
+for d in 180 120 167 152 719; do
+    docker-compose run imagemagick convert homescreen.png -resize ${d}x${d} -background white build/images/touch/apple-touch-icon-${d}.png;
+done
