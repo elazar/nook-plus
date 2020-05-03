dd67ab712b36a3cb55eb606ea45ff1c0ef25eb60
elazar <tobias382@gmail.com>
2020-05-03 16:55:10 -0500

    Modify navigation in Layout to use Mithril routing

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -18,7 +18,7 @@ const Layout = content => (
             <ul className="flex flex-row flex-wrap justify-center mb-8">
                 { pages.map(page => (
                     <li className="mr-4 mt-4">
-                        <a className="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" href={ `#!${page.path}` }>{ page.name }</a>
+                        <a className="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" onclick={ () => m.route.set(page.path) }>{ page.name }</a>
                     </li>
                 )) }
             </ul>
