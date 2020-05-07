39adc2a55e3795998e0a5bbe02eff48655d4240a
elazar <tobias382@gmail.com>
2020-05-07 17:11:47 -0500

    Update Furniture data

--- scripts/furniture.php
+++ scripts/furniture.php
@@ -7,9 +7,6 @@ use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpClient\HttpClient;
 
 $browser = new HttpBrowser(HttpClient::create());
-$browser->request('GET', 'https://animalcrossing.fandom.com/wiki/Furniture_(New_Horizons)');
-$crawler = $browser->getCrawler();
-$tables = $crawler->filter('table.roundy');
 
 $parse_price = function ($text) {
     if ($text === 'N/A') {
@@ -33,50 +30,65 @@ $format_longform = function ($value) {
     return $value;
 };
 
+$subpages = [
+    'Housewares',
+    'Miscellaneous',
+    'Wall-mounted',
+    'Wallpaper',
+    'Flooring',
+    'Rugs',
+];
+
 $rows = [];
 
-foreach ($tables as $table) {
-    $trs = (new Crawler($table))->filter('tr');
-    if (count($trs) < 2) {
-        continue;
-    }
+foreach ($subpages as $subpage) {
+    $browser->request('GET', 'https://animalcrossing.fandom.com/wiki/Furniture_(New_Horizons)/' . $subpage);
+    $crawler = $browser->getCrawler();
+    $tables = $crawler->filter('table.roundy');
 
-    foreach ($trs as $tr) {
-        $tr = new Crawler($tr);
-        $td = $tr->filter('td');
-        $td_count = count($td);
-        if ($td_count < 5) {
+    foreach ($tables as $table) {
+        $trs = (new Crawler($table))->filter('tr');
+        if (count($trs) < 2) {
             continue;
         }
 
-        $row = new stdClass;
+        foreach ($trs as $tr) {
+            $tr = new Crawler($tr);
+            $td = $tr->filter('td');
+            $td_count = count($td);
+            if ($td_count < 5) {
+                continue;
+            }
 
-        $urls = $td->eq(0)->filter('img')->eq(0)->extract(['src', 'data-src'])[0];
-        $row->image = empty($urls) ? null : reset(array_filter($urls, $url_data));
+            $row = new stdClass;
 
-        $row->name = $td->eq(1)->text();
+            $urls = $td->eq(0)->filter('img')->eq(0)->extract(['src', 'data-src'])[0];
+            $row->image = empty($urls) ? null : reset(array_filter($urls, $url_data));
 
-        $link = $td->eq(1)->filter('a')->eq(0)->extract(['href'])[0];
-        $row->link = $link ? 'https://animalcrossing.fandom.com' . $link : null;
+            $row->name = $td->eq(1)->text();
 
-        $row->buy_price = $parse_price($td->eq(2)->text());
-        $row->sell_price = $parse_price($td->eq(3)->text());
+            $link = $td->eq(1)->filter('a')->eq(0)->extract(['href'])[0];
+            $row->link = $link ? 'https://animalcrossing.fandom.com' . $link : null;
 
-        $link = $td->eq(4)->filter('a');
-        if (count($link) > 0) {
-            $row->source = $link->eq(0)->extract(['title'])[0];
+            $row->buy_price = $parse_price($td->eq(2)->text());
+            $row->sell_price = $parse_price($td->eq(3)->text());
+
+            $link = $td->eq(4)->filter('a');
+            if (count($link) > 0) {
+                $row->source = $link->eq(0)->extract(['title'])[0];
+            }
+
+            if ($td_count === 8) {
+                $variations = $nullify($td->eq(5)->text());
+                $row->variations = $variations ? $format_longform($variations) : null;
+
+                $customize = $nullify($td->eq(6)->text());
+                $row->customize = $customize ? $format_longform($customize) : null;
+            }
+
+            $rows[] = $row;
         }
-
-        if ($td_count === 8) {
-            $variations = $nullify($td->eq(5)->text());
-            $row->variations = $variations ? $format_longform($variations) : null;
-
-            $customize = $nullify($td->eq(6)->text());
-            $row->customize = $customize ? $format_longform($customize) : null;
-        }
-
-        $rows[] = $row;
     }
 }
 
-echo json_encode($rows);
+echo json_encode($rows, \JSON_PRETTY_PRINT);
